01f92a6a9847a88f2233cb368468cadc52cf4581
benedek.csikos <csikosbenedek@gmail.com>
2024-09-09 22:23:56 +0200

    product list & header & footer done

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,24 +1,27 @@
 import { Component } from '@angular/core';
 import { ProductService } from '../../services/product.service';
-import { Products } from '../../../types';
+import { Product, Products } from '../../../types';
+import { ProductComponent } from '../product/product.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ProductComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
   constructor(private productService: ProductService) {}
 
+  products: Product[] = [];
+
   ngOnInit(): void {
-    console.log('asd');
     this.productService
       .getProducts('http://localhost:3000/clothes', {
         page: 0,
         perPage: 5,
       })
-      .subscribe((products: Products) => console.log(products));
+      .subscribe((products: Products) => (this.products = products.items));
   }
 }
