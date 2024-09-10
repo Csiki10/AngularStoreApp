9f3563bfc5255e9d6432bd8d5b6ccf6fb99b01e9
benedek.csikos <csikosbenedek@gmail.com>
2024-09-10 10:46:47 +0200

    add delte edit api endpoint implemented

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,11 +3,12 @@ import { ProductService } from '../../services/product.service';
 import { Product, Products } from '../../../types';
 import { ProductComponent } from '../product/product.component';
 import { CommonModule } from '@angular/common';
+import { PaginatorModule } from 'primeng/paginator';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ProductComponent, CommonModule],
+  imports: [ProductComponent, CommonModule, PaginatorModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
@@ -15,13 +16,68 @@ export class HomeComponent {
   constructor(private productService: ProductService) {}
 
   products: Product[] = [];
+  totalRecords: number = 0;
+  rows: number = 5;
+  apiUrl: string = 'http://localhost:3000/clothes';
 
   ngOnInit(): void {
+    this.fectProducts(0, this.rows);
+  }
+
+  onPageChange(event: any) {
+    this.fectProducts(event.page, event.rows);
+  }
+
+  fectProducts(page: number, perPage: number) {
     this.productService
-      .getProducts('http://localhost:3000/clothes', {
-        page: 0,
-        perPage: 5,
+      .getProducts(this.apiUrl, {
+        page: page,
+        perPage: perPage,
       })
-      .subscribe((products: Products) => (this.products = products.items));
+      .subscribe({
+        next: (products: Products) => {
+          this.products = products.items;
+          this.totalRecords = products.total;
+        },
+        error: (error) => {
+          console.error('Error fetching products:', error);
+        },
+      });
+  }
+
+  editProduct(product: Product, id: number) {
+    this.productService.editProduct(this.apiUrl + '/' + id, product).subscribe({
+      next: () => {
+        console.log('Product updated successfully');
+        this.fectProducts(0, this.rows);
+      },
+      error: (error) => {
+        console.error('Error updating product:', error);
+      },
+    });
+  }
+
+  addProduct(product: Product) {
+    this.productService.addProduct(this.apiUrl, product).subscribe({
+      next: () => {
+        console.log('Product added successfully');
+        this.fectProducts(0, this.rows);
+      },
+      error: (error) => {
+        console.error('Error adding product:', error);
+      },
+    });
+  }
+
+  deleteProduct(id: number) {
+    this.productService.deleteProduct(this.apiUrl + '/' + id).subscribe({
+      next: () => {
+        console.log('Product deleted successfully');
+        this.fectProducts(0, this.rows);
+      },
+      error: (error) => {
+        console.error('Error deleting product:', error);
+      },
+    });
   }
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { ApiService } from './api.service';
 import { Observable } from 'rxjs';
-import { PaginationParams, Products } from '../../types';
+import { PaginationParams, Product, Products } from '../../types';
 
 @Injectable({
   providedIn: 'root',
@@ -15,6 +15,24 @@ export class ProductService {
   ): Observable<Products> => {
     return this.apiService.get(url, {
       params: params,
+      responseType: 'json',
+    });
+  };
+
+  addProduct = (url: string, body: Product): Observable<Product> => {
+    return this.apiService.post(url, body, {
+      responseType: 'json',
+    });
+  };
+
+  editProduct = (url: string, body: Product): Observable<Product> => {
+    return this.apiService.put(url, body, {
+      responseType: 'json',
+    });
+  };
+
+  deleteProduct = (url: string): Observable<Product> => {
+    return this.apiService.delete(url, {
       responseType: 'json',
     });
   };
